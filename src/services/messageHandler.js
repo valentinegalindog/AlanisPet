b94c0baa55e85ac5d4c419246e1179c0a6926497
valentinegalindog <valentine.galindog@utadeo.edu.co>
2025-05-12 11:24:56 -0500

    primer despliegue chat bot

---
+++ src/services/messageHandler.js
@@ -0,0 +1,75 @@
+import whatsappService from './whatsappService.js';
+
+class MessageHandler {
+  async handleIncomingMessage(message, senderInfo) {
+    if (message?.type === 'text') {
+      const incomingMessage = message.text.body.toLowerCase().trim();
+
+      if(this.isGreeting(incomingMessage)){
+        await this.sendWelcomeMessage(message.from, message.id, senderInfo);
+        await this.sendWelcomeMenu(message.from);
+      } else {
+        const response = `Echo: ${message.text.body}`;
+        await whatsappService.sendMessage(message.from, response, message.id);
+      }
+      await whatsappService.markAsRead(message.id);
+    } else if (message?.type === 'interactive') {
+        const option = message?.interactive?.button_reply?.title.toLowerCase().trim();
+        await this.handleMenuOption(message.from, option);
+        await whatsappService.markAsRead(message.id);
+    }
+  }
+
+  isGreeting(message) {
+    const greetings = ["hola", "hello", "hi", "buenas tardes"];
+    return greetings.includes(message);
+  }
+
+  getSenderName(senderInfo) {
+    return senderInfo.profile?.name || senderInfo.wa_id;
+  }
+
+  async sendWelcomeMessage(to, messageId, senderInfo) {
+    const name = this.getSenderName(senderInfo);
+    const welcomeMessage = `Hola ${name}, Bienvenido a AlanisPet, tu clínica veterinaria de confianza. Soy Alanis, tu asistente virtual, creada por la Universidad Jorge Tadeo Lozano, y te ayudaré con tu solicitud. ¿En qué puedo ayudarte hoy?`;
+    await whatsappService.sendMessage(to, welcomeMessage, messageId);
+  }
+
+  async sendWelcomeMenu(to) {
+    const menuMessage = "Elige una Opción"
+    const buttons = [
+      {
+        type: 'reply', reply: { id: 'option_1', title: 'Agendar' }
+      },
+      {
+        type: 'reply', reply: { id: 'option_2', title: 'Consultar'}
+      },
+      {
+        type: 'reply', reply: { id: 'option_3', title: 'Ubicación'}
+      }
+    ];
+
+    await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
+  }
+
+  async handleMenuOption(to, option) {
+    let response;
+    switch (option) {
+        case 'agendar':
+            response = 'Agendar Cita';
+            break;
+        case 'consultar':
+            response = 'Realiza tu consulta';
+            break;
+        case 'ubicacion':
+            response = "Esta es nuestra ubicación";
+            break;
+        default:
+            response = 'Lo siento, no entendí tu selección, Por Favor, elige una de las opciones del menú'
+    }
+    await whatsappService.sendMessage(to, response)
+  }
+
+}
+
+export default new MessageHandler();
---
+++ src/services/whatsappService.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import config from '../config/env.js';
+
+class WhatsAppService {
+  async sendMessage(to, body, messageId) {
+    try {
+      await axios({
+        method: 'POST',
+        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        headers: {
+          Authorization: `Bearer ${config.API_TOKEN}`,
+        },
+        data: {
+          messaging_product: 'whatsapp',
+          to,
+          text: { body },
+        // context: {
+            // message_id: messageId,
+          //},
+        },
+      });
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  }
+
+  async markAsRead(messageId) {
+    try {
+      await axios({
+        method: 'POST',
+        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        headers: {
+          Authorization: `Bearer ${config.API_TOKEN}`,
+        },
+        data: {
+          messaging_product: 'whatsapp',
+          status: 'read',
+          message_id: messageId,
+        },
+      });
+    } catch (error) {
+      console.error('Error marking message as read:', error);
+    }
+  }
+
+  async sendInteractiveButtons(to, BodyText, buttons) {
+    try {
+      await axios({
+        method: 'POST',
+        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        headers: {
+          Authorization: `Bearer ${config.API_TOKEN}`,
+        },
+        data: {
+          messaging_product: 'whatsapp',
+          to,
+          type: 'interactive',
+          interactive: {
+            type: 'button',
+            body: { text: BodyText },
+            action: {
+              buttons: buttons
+            }
+          }
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+}
+
+export default new WhatsAppService();
